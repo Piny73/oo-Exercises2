b9b9f4565a5184874a6e0cc402cf0438157821a5
Giuseppina Salemme <Piny73@gmail.com>
2024-03-18 16:58:38 +0100

    aggio'

--- demogson/demogson/src/main/java/samplegson/Main.java
+++ demogson/demogson/src/main/java/samplegson/Main.java
@@ -7,18 +7,19 @@ import com.google.gson.GsonBuilder;
 
 public class Main {
     public static void main(String[] args) {
-
-        Partecipanti p1 = new Partecipanti("Salemme", "Giuseppina", "giussal@gmail.com", "3480087652");
+        Partecipanti p;
+        p = new Partecipanti("Salemme", "Giuseppina", "giussal@gmail.com", "3480087652");
+        p = new Partecipanti("Dente", "Massimiliano", "massden@gmail.com", "3480087331");
         GsonBuilder gsonb = new GsonBuilder();
 
         gsonb.registerTypeAdapter(LocalDateTime.class, new LocalDateTimeSerializer());
         gsonb.registerTypeAdapter(LocalDateTime.class, new LocalDateTimeDeserializer());
         Gson gson = gsonb.setPrettyPrinting().create();
-        String p1TxJson = gson.toJson(p1);
+        String p1TxJson = gson.toJson(p);
         System.out.println(p1TxJson);
 
         Evento event = new Evento("Concerto di musica da camera", "Ravello");
-        event.addIdPartecipant(p1.getId());
+        event.addIdPartecipant(p.getId());
         String evTxJson = gson.toJson(event);
         System.out.println(evTxJson);
         Evento event2 = gson.fromJson(evTxJson, Evento.class);
