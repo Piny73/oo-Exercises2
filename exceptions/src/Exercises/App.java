1852ea8478aba48888ca3935a956a02952ad9e05
Giuseppina Salemme <Piny73@gmail.com>
2024-04-04 10:41:34 +0200

    agg

--- exceptions/src/Exercises/App.java
+++ exceptions/src/Exercises/App.java
@@ -11,27 +11,24 @@ import java.util.List;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) {        
-           
-            Scanner s = new Scanner (System.in);
-            boolean error = false;
-            List<String> lines = new ArrayList<>();
-            do {
-                System.out.println("dimmi il percorso del file da leggere");
-                String filename = s.nextLine();       
-            
-                try {
-                    lines = Files.readAllLines(Paths.get(filename));
-                }catch (IOException e){
-                    System.out.println("Il file non esiste!");
-                    error = true;
-                }
-            }while(error);
+    public static void main(String[] args) {
 
-            lines.forEach(v -> System.out.println(v));
-
-            System.out.println("IL PROGRAMMA TERMINA CORRETTAMENTE");
+        Scanner s = new Scanner(System.in);
+        boolean error = false;
+        List<String> lines = new ArrayList<>();
+        do {
+            System.out.println("dimmi il percorso del file da leggere");
+            String filename = s.nextLine();
+            try {
+                lines = Files.readAllLines(Paths.get(filename));
+            } catch (IOException e) {
+                System.out.println("Il file non esiste!");
+                error = true;
+            }
+        } while (error);
+        lines.forEach(v -> System.out.println(v));
+        System.out.println("IL PROGRAMMA TERMINA CORRETTAMENTE");
     }
 }
-//c:\\Users\\utentepc\\Documents\\Except.txt
-//C:\\Users\\utentepc\\Documents\\Except.txt
+// c:\\Users\\utentepc\\Documents\\Except.txt
+// C:\\Users\\utentepc\\Documents\\Except.txt
