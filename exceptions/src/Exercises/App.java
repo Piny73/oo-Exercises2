db9284c266b17ec7cad8e22d7ca162fb5d484757
Giuseppina Salemme <Piny73@gmail.com>
2024-04-04 10:40:06 +0200

    aggio

--- exceptions/src/Exercises/App.java
+++ exceptions/src/Exercises/App.java
@@ -1,10 +1,37 @@
 package Exercises;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.FileSystemNotFoundException;
 import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        Files.readAllLines(Path.get("c:\\tmp\\prova.txt"));
+    public static void main(String[] args) {        
+           
+            Scanner s = new Scanner (System.in);
+            boolean error = false;
+            List<String> lines = new ArrayList<>();
+            do {
+                System.out.println("dimmi il percorso del file da leggere");
+                String filename = s.nextLine();       
+            
+                try {
+                    lines = Files.readAllLines(Paths.get(filename));
+                }catch (IOException e){
+                    System.out.println("Il file non esiste!");
+                    error = true;
+                }
+            }while(error);
+
+            lines.forEach(v -> System.out.println(v));
+
+            System.out.println("IL PROGRAMMA TERMINA CORRETTAMENTE");
     }
 }
+//c:\\Users\\utentepc\\Documents\\Except.txt
+//C:\\Users\\utentepc\\Documents\\Except.txt
